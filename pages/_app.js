5927ee6cacbe41e78ed80255e6b3514fabc029dc
George Edwards <georgeedwardsmail@gmail.com>
2022-03-13 23:48:09 +1100

    custom <head> added, favicon now works

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout/Layout'
 import { AboutModalProvider } from '../components/About/AboutModalContext'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AboutModalProvider>
-      <Layout />
-    </AboutModalProvider>
+    <>
+      <Head>
+        <title>is it down?</title>
+      </Head>
+      <AboutModalProvider>
+        <Layout />
+      </AboutModalProvider>
+    </>
   )
 }
 
