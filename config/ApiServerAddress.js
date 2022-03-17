a2f7e9c2c93a37360d24e0b0f0b5b8836de54b3f
George Edwards <georgeedwardsmail@gmail.com>
2022-03-17 17:17:01 +1100

    added log.json to project root

--- config/ApiServerAddress.js
+++ config/ApiServerAddress.js
@@ -1,4 +1,6 @@
 const dev = process.env.NODE_ENV !== 'production'
 
 export const apiserver = dev ? 'http://localhost:3000' : 'https://isitdown.it.csiro.au';
-export const serverLogFile = dev? 'C:\\Users\\edw19b\\OneDrive - CSIRO\\Documents\\isitdown-serverlog\\log.json' : './log.json';
+export const serverLogFile = dev ? 
+        'C:\\Users\\edw19b\\OneDrive - CSIRO\\Documents\\isitdown-serverlog\\log.json' : 
+        '/home/edw19b/is-it-down/log.json';
